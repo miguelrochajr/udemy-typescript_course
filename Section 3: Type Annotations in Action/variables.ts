fd8312be31dad8f29e771a70e448ceb624d83fab
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2020-12-25 20:48:07 -0300

    section 3

--- Section 3: Type Annotations in Action/variables.ts
+++ Section 3: Type Annotations in Action/variables.ts
@@ -29,3 +29,37 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function.
+// it really looks like Swift. Damn, Apple really did great in that language
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Fucntion that eturns the 'any' type
+const json = '{"x": 10, "y": 10}';
+// JSON.parse returns 'any'. See video 19 at 4"20'
+// .parse can return different types depending on the input's type
+const coordinats: { x: number; y: number } = JSON.parse(json);
+console.log(coordinats);
+
+// 2) When we declarea variable on one line
+// and initalize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // is always best to initialize it first
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // boolean OR number types
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
