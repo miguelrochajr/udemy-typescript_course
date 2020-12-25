bb274399b5413447914655826fa9087d8ff9b238
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2020-12-25 20:04:27 -0300

    section 3

---
+++ Section 2: What is a Type Script System/features/types.ts
@@ -0,0 +1,16 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {
+  constructor(hex: string) {
+    this.hex = hex;
+  }
+
+  hex = null;
+}
+
+const red = new Color('#45543');
