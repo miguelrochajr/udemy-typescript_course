91583197b0f06982611e60f112af51cbd3d32eee
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2020-12-26 01:02:06 -0300

    started working with google maps

---
+++ Section 9: Design Patterns with Typescript/maps/src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+const company = new Company();
+new CustomMap('map');
+
+console.log(user);
+console.log(company);
