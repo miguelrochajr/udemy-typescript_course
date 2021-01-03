0b91b2960abf9b2461fd19f5ab1db1f21248df6a
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2021-01-03 08:56:39 -0300

    decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,17 +1,51 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color ir ${this.color}`;
   }
 
-  @testDecorator
-  pilot(): void {
-    console.log('swish');
+  @logError('Ooops. The boat has sunk.')
+  pilot(@parameterDecorator speed: string): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
 }
 
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+// The target is ALWAYS the prototype
+function parameterDecorator(target: any, key: string, index: number): void {
+  console.log(key, index);
+}
+
 function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
+  console.log('Target: ', target);
   console.log('Key: ', key);
+}
+
+// decorator factory
+function logError(errorMessage: string) {
+  return function logError(
+    target: any,
+    key: string,
+    desc: PropertyDescriptor
+  ): void {
+    const method = desc.value;
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
