d6f27fb9581b945ac6a8207762b3cc49bca1edfa
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2021-05-02 17:52:41 -0300

    removed rrts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # udemy-typescript_course
-udem typescript course
+
+udemy typescript course
+
+# Section 17: React and Redux with Typescript
+
+Uses the project named _rrts_, which is short for React Redux Typescript
--- rrts/src/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import { FetchTodosAction, DeleteTodoAction } from './todos';
-
-export enum ActionTypes {
-  fetchTodos, // typescript assig ns a 0 to this.
-  deleteTodo,
-}
-
-export type Action = FetchTodosAction | DeleteTodoAction;
